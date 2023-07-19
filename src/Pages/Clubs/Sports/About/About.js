3e6cbf1ceef81732b8006d04d00d2439e4a7338c
Mayank Kumar <mayank.k9802@gmail.com>
2023-07-19 23:27:34 +0530

    Update About.js

--- src/Pages/Clubs/Sports/About/About.js
+++ src/Pages/Clubs/Sports/About/About.js
@@ -1,22 +1,28 @@
-import React from 'react';
-import './SportsAbout.css';
-function About () {
-    return (
+import React from "react";
+import "./SportsAbout.css";
+function About() {
+  return (
     <div className="about-content">
-        
-        <div className="box">
-            <div id='AboutSports' className="about-title">
-            About the Club
+      <div className="box">
+        <div id="AboutSports" className="about-title">
+          About the Club
         </div>
-   <div className="about-text">
-    <p>Welcome to the Sports Club of JUIT: The Ultimate Destination for Sports Enthusiasts! Our highly active and engaging club hosts a variety of sports events, ensuring students find a well-deserved break from their studies. From Semester Tournaments to the exhilarating Parakram inter-university extravaganza, our events provide a perfect blend of competition and enjoyment. Join us to experience the thrill of sports, forge lifelong connections, and be part of a vibrant sporting community. Discover the power of sports at the Sports Club of JUIT and unlock your athletic potential today!
-    </p>
+        <div className="about-text">
+          <p>
+            Welcome to the Sports Club of JUIT: The Ultimate Destination for
+            Sports Enthusiasts! Our highly active and engaging club hosts a
+            variety of sports events, ensuring students find a well-deserved
+            break from their studies. From Semester Tournaments to the
+            exhilarating Parakram inter-university extravaganza, our events
+            provide a perfect blend of competition and enjoyment. Join us to
+            experience the thrill of sports, forge lifelong connections, and be
+            part of a vibrant sporting community. Discover the power of sports
+            at the Sports Club of JUIT and unlock your athletic potential today!
+          </p>
+        </div>
+      </div>
     </div>
-    </div>
-    </div>
-    
-
-    );
+  );
 }
 
 export default About;
